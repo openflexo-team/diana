fdb84e1c1191af87525f59ac4ab06881f7bff803
Sylvain Guérin <sylvain@openflexo.org>
2014-06-11 16:01:20 +0200

    MEDIUM / Added unit tests

--- diana-swing/src/test/java/org/openflexo/fge/fib/TestDianaFibs.java
+++ diana-swing/src/test/java/org/openflexo/fge/fib/TestDianaFibs.java
@@ -2,6 +2,7 @@ package org.openflexo.fge.fib;
 
 import java.io.File;
 
+import org.junit.Test;
 import org.openflexo.fib.utils.GenericFIBTestCase;
 
 public class TestDianaFibs extends GenericFIBTestCase {
@@ -10,22 +11,27 @@ public class TestDianaFibs extends GenericFIBTestCase {
 		System.out.println(generateFIBTestCaseClass(new File(System.getProperty("user.dir") + "/src/main/resources/Fib"), "Fib/"));
 	}
 
+	@Test
 	public void testBackgroundStylePanel() {
 		validateFIB("Fib/BackgroundStylePanel.fib");
 	}
 
+	@Test
 	public void testForegroundStylePanel() {
 		validateFIB("Fib/ForegroundStylePanel.fib");
 	}
 
+	@Test
 	public void testShadowStylePanel() {
 		validateFIB("Fib/ShadowStylePanel.fib");
 	}
 
+	@Test
 	public void testShapeSelectorPanel() {
 		validateFIB("Fib/ShapeSelectorPanel.fib");
 	}
 
+	@Test
 	public void testTextStylePanel() {
 		validateFIB("Fib/TextStylePanel.fib");
 	}
