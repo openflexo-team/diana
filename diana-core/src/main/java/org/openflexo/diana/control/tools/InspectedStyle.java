2eeb9c4ae460906837beb947da1ca057375979df
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2021-11-18 22:41:33 +0100

    LOW / Wildcard for ModelProperty<?> in equals() method

--- diana-core/src/main/java/org/openflexo/diana/control/tools/InspectedStyle.java
+++ diana-core/src/main/java/org/openflexo/diana/control/tools/InspectedStyle.java
@@ -600,7 +600,7 @@ public abstract class InspectedStyle<S extends KeyValueCoding> implements HasPro
 		return false;
 	}
 
-	public boolean equalsObject(Object obj, Function<ModelProperty, Boolean> considerProperty) {
+	public boolean equalsObject(Object obj, Function<ModelProperty<?>, Boolean> considerProperty) {
 		return equals(obj);
 	}
 
