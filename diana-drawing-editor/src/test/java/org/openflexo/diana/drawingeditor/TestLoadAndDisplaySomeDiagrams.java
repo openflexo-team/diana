f4172d78eeca470993a103d7af436528037e9363
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2022-09-28 11:31:54 +0200

    LOW / "Un-ignore" UI test causing job blocking

--- diana-drawing-editor/src/test/java/org/openflexo/diana/drawingeditor/TestLoadAndDisplaySomeDiagrams.java
+++ diana-drawing-editor/src/test/java/org/openflexo/diana/drawingeditor/TestLoadAndDisplaySomeDiagrams.java
@@ -53,7 +53,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openflexo.diana.drawingeditor.model.DiagramFactory;
@@ -63,7 +62,7 @@ import org.openflexo.pamela.factory.EditingContextImpl;
 import org.openflexo.rm.ResourceLocator;
 import org.openflexo.test.UITest;
 
-@Ignore
+//@Ignore
 public class TestLoadAndDisplaySomeDiagrams {
 
 	public static EventProcessor eventProcessor;
